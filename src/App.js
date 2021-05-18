9193defd9a52635700798e606dfd1873fee95900
ui-cod-tv <uicodetv@googlemail.com>
2021-05-18 22:55:51 +0100

    firts rev

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+function LoginPage({ handleSubmit }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form id="login-form" onSubmit={handleSubmit}>
+      <label>
+        Email:
+        <input className="input" type="email" placeholder="me@you.com" />
+      </label>
+      <label>
+        Password:
+        <input className="input" type="password" />
+      </label>
+      <button type="submit">Log In</button>
+    </form>
   );
 }
 
-export default App;
+function ProtectedPage({ handleSubmit }) {
+  return (
+    <>
+      <h2>Protected courses page</h2>
+      <p>You are in the members area of this site</p>
+      <button class="primary">Add Course</button>{" "}
+      <button class="secondary">Remove Course</button>
+      <main class="container">
+        <div class="card">
+          <div class="img"></div>
+          <div class="content">
+            <h3>Course Name</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <button class="small">more details...</button>
+          </div>
+        </div>
+        <div class="card">
+          <div class="img"></div>
+          <div class="content">
+            <h3>Course Name</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <button class="small">more details...</button>
+          </div>
+        </div>
+        <div class="card">
+          <div class="img"></div>
+          <div class="content">
+            <h3>Course Name</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <button class="small">more details...</button>
+          </div>
+        </div>
+        <div class="card">
+          <div class="img"></div>
+          <div class="content">
+            <h3>Course Name</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <button class="small">more details...</button>
+          </div>
+        </div>
+        <div class="card">
+          <div class="img"></div>
+          <div class="content">
+            <h3>Course Name</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <button class="small">more details...</button>
+          </div>
+        </div>
+        <div class="card">
+          <div class="img"></div>
+          <div class="content">
+            <h3>Course Name</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+            <button class="small">more details...</button>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
+export default function App() {
+  const [isloggedIn, setIsLoggedIn] = useState(false);
+  if (isloggedIn) {
+    return (
+      <>
+        <ProtectedPage />
+      </>
+    );
+  }
+  return (
+    <LoginPage
+      handleSubmit={(ev) => {
+        ev.preventDefault();
+        setIsLoggedIn(true);
+      }}
+    />
+  );
+
+}
+
